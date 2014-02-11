e5fae0de4b1877a7db077111a06bea47fd2f50bc
borazslo <eleklaszlosj@gmail.com>
2014-02-11 11:44:00 +0100

    index.php kommentelse

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php 
-//require_once('/var/www/szentiras.hu/include/php-ga-1.1.1/src/autoload.php');
-//use UnitedPrototype\GoogleAnalytics;
-// Initilize GA Tracker
-//$tracker = new GoogleAnalytics\Tracker('UA-36302080-1', 'szentiras.hu');
-// Assemble Visitor information
-// (could also get unserialized from database)
-//$visitor = new GoogleAnalytics\Visitor();
-// Assemble Session information
-// (could also get unserialized from PHP session)
-//$session = new GoogleAnalytics\Session();
-
-//$event = new GoogleAnalytics\Event('','','');
-
 session_start();
-header('Content-type: text/html; charset=utf-8'); 
 
 require_once('bibleconf.php');
-require_once("biblefunc.php");
+require_once('biblefunc.php');
 require_once('func.php');
 require_once('quote.php');
 
 
-/* Hosszú URL átirányítás RÖVIDRE */
-
+/**Hosszú URL átirányítás RÖVIDRE**/
+/* Arra is gondol, hogy ha régi-régi url-t kap, mert valaki még arra 
+   hivatkozik, akkor az is átjusson a megfelelő oldalra. 
+*/   
 redirect_long2short();
 
+/**CACHE**/
+/* Elég agresszív, de a túlélés miatt csináltam.
+   A végén van egy kis kód még, ami kell hozzá. (Az ment.) 
+*/
+$uri = $_SERVER["REQUEST_URI"];
+$cachefile = 'cache/cached-'.md5($uri).'.html';
+$cachetime = 18000;
+$cachetime = 18000;
 
-/* */
+if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
+    echo "<!-- Cached copy, generated ".date('Y-m-d H:i', filemtime($cachefile))." -->\n";
+    include($cachefile);
+    exit;
+}
+ob_start(); // Start the output buffer
+/**/
 
+header('Content-type: text/html; charset=utf-8'); 
+
+/* Néhány változó alapértelmezett beállítása */
 if(isset($_REQUEST['reftrans']) AND is_numeric($_REQUEST['reftrans']) AND $_REQUEST['reftrans'] > 0 ) $reftrans = $_REQUEST['reftrans'];
 else $reftrans = 1;
 if(isset($_REQUEST['q'])) $q = $_REQUEST['q'];
@@ -35,12 +40,15 @@ else $q = 'showbible';
 if(isset($_REQUEST['texttosearch']) AND $_REQUEST['texttosearch'] != '') $texttosearch = $_REQUEST['texttosearch'];
 else $texttosearch = false; 
 
+/**Rövid URL-ből kiszedjük a változókat**/
 $vars = url_short2vars();
 foreach($vars as $k=>$v) $$k = $v;
-if(isset($transid)) $reftrans = $transid;
-//print_r($vars);
 
-//echo $q;
+//Sajnos a $transid $reftrans redundáns definíció
+if(isset($transid)) $reftrans = $transid;
+
+//A $q változó mondja meg, hogy mit is akarunk betölteni.
+//Kb. minden típusú oldal egy-egy $q file tartozik hozzá.
 if($q=='showbible') $hirek = getnews();
 if($q != false AND file_exists($q.'.php')) require_once($q.'.php');
 else {
@@ -48,8 +56,11 @@ else {
 	$content = 'Elnézést kérünk a kellemetlenségért.<br/>';
 }
                      
+//Talán csak a statisztikáknál használjuk. Talán.
 if(!isset($original) OR $original == '') $original = print_R($_REQUEST,1);
 
+
+/**MENÜ felépítése**/
 $menu = new Menu();
 	//$menu->add_item("Bibliaolvasás","showbible");
 	//$menu->add_item("Bibliaolvasás",BASE);
@@ -88,10 +99,13 @@ $menu = new Menu();
 	$menu->add_item("Görög újszövetségi honlap","http://www.ujszov.hu/");
 	$menu->add_item("Katolikus igenaptár","http://www.katolikus.hu/igenaptar/");
 	$menu->add_item("Zsolozsma","http://zsolozsma.katolikus.hu/");
+/**/	
+	
 	
 	$abbrevlist = showbookabbrevlist($reftrans,"");
 	
-	/* URL-ek átírása a szövegekben */
+/** URL-ek átírása a szövegekben **/
+/* A $content, $title, $abbrevlist-ban előfordulhatnak véletlenül hosszú url-ek */
 	foreach(array('content','title','abbrevlist') as $var) {
 		$text = $$var;
 		$pattern = '/(\\\'|")'.addcslashes(BASE,'./').'index\.php\?q=showtrans&reftrans=([0-9]{1,2})(\\\'|")/i';
@@ -106,7 +120,11 @@ $menu = new Menu();
 		
 		$$var = $text;
 	}
-	
+/**/
+
+
+/**SEGÉD FÜGGVÉNYEK**/
+/* Ugyan, miért itt vannak ezek? */	
 	function url_showtrans($m) {
 			global $translationIDs;
 			return $m[1].BASE.$translationIDs[$m[2]]['abbrev'].$m[1];
@@ -125,15 +143,18 @@ $menu = new Menu();
 			if(!isset($m[5])) $m[5] = '';
 			return $m[1].BASE.$translationIDs[$m[3]]['abbrev']."/".$m[2].$m[4].$m[5].$m[1];
 		}
-	/*
-	$content = preg_replace('/biblia\/([a-z]*?)\.php(\?|)/','biblia2/INDEX?q=$1&',$content);
-	$content = preg_replace('/(=\'|=")([a-z]*?)\.php(\?|)/','$1INDEX?q=$2&',$content);
-	$content = preg_replace('/(=\'|=")(http:\/\/www\.kereszteny\.hu\/biblia2\/)(.*?)\.php\?(.*?)(\'|")/i','$1$2INDEX?q=$3&$4$5',$content);
-	$content = preg_replace('/INDEX/s','index.php',$content);
-	*/
-	
-	//$meta .= '<meta property="og:description" content="The Rock" />';
+/**/
 
+/**Akkor kiírjuk azt, ami eddig bejött.**/
 include 'template.php';	
+
+
+/**CACHE vége**/
+/* Cacha mentése file-ban*/
+$cached = fopen($cachefile, 'w');
+fwrite($cached, ob_get_contents());
+fclose($cached);
+ob_end_flush(); // Send the output to the browser
+/**/
 ?>
  
