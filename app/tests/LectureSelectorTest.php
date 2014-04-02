982593570adc2c65f306a9a045df16c701abe483
Bertalan Fodor <fodber@organum.hu>
2014-04-02 17:16:45 +0200

    Introduce mocking for lecture downloader in test.

---
+++ app/tests/LectureSelectorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class LectureSelectorTest extends TestCase {
+
+    private $downloader;
+
+    public function setUp() {
+        parent::setUp();
+        $this->downloader = Mockery::mock('LectureDownloader');
+        $this->app->instance('LectureDownloader', $this->downloader);
+    }
+
+    public function testEmpty() {
+        $this->downloader->shouldReceive('getReferenceString')->andReturn("");
+        $selector = new LectureSelector("20140403");
+        $lectures = $selector->getLectures();
+        $this->assertEmpty($lectures);
+    }
+
+    public function testSimple() {
+        $this->downloader->shouldReceive('getReferenceString')->andReturn('1Ter 4,5; 1Jn 2-3');
+        $selector = new LectureSelector("");
+        $lectures = $selector->getLectures();
+        $this->assertCount(2, $lectures);
+    }
+
+} 
