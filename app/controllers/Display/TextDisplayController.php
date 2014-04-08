76f527e32dba5c162b9bb72970017b84119b8903
Bertalan Fodor <fodber@organum.hu>
2014-04-08 12:38:15 +0200

    Display first two verses in books

--- app/controllers/Display/TextDisplayController.php
+++ app/controllers/Display/TextDisplayController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace SzentirasHu\Controllers\Display;
+
 use SzentirasHu\Lib\Reference\CanonicalReference;
 use SzentirasHu\Models\Entities\Book;
 use SzentirasHu\Models\Entities\Translation;
@@ -9,34 +10,49 @@ use SzentirasHu\Models\Entities\Translation;
  *
  * @author berti
  */
-class TextDisplayController extends \BaseController {
-    
-    public function showTranslation($translationAbbrev) {
+class TextDisplayController extends \BaseController
+{
+
+    public function showTranslation($translationAbbrev)
+    {
         $translation = Translation::where('abbrev', $translationAbbrev)->first();
         $books = $translation->books()->orderBy('id')->get();
         return \View::make('textDisplay.translation',
             ['translation' => $translation,
-            'books' => $books]);
+                'books' => $books]);
     }
-    
-    public function showReferenceText($reference) {
+
+    public function showReferenceText($reference)
+    {
         return $this->showTranslatedReferenceText(\Config::get('settings.defaultTranslationAbbrev'), $reference);
     }
-    
-    public function showTranslatedReferenceText($translationAbbrev, $reference) {
+
+    public function showTranslatedReferenceText($translationAbbrev, $reference)
+    {
         $canonicalRef = CanonicalReference::fromString($reference);
         $bookRef = $canonicalRef->bookRefs[0];
         $translation = Translation::where('abbrev', $translationAbbrev)->first();
         $translatedRef = $canonicalRef->toTranslated($translation->id);
         $book = Book::where('abbrev', $translatedRef->bookRefs[0]->bookId)->where('translation_id', $translation->id)->first();
-        $firstVerses = $book->verses()->where('trans', $translation->id)->where('numv','1')->where('tip',6)->orderBy('chapter')->get();
+        $firstVerses = $book
+            ->verses()
+            ->where('trans', $translation->id)
+            ->whereIn('numv', ['1', '2'])
+            ->where('tip', 6)
+            ->orderBy('chapter')
+            ->orderBy('numv')
+            ->get();
+        $groupedVerses = [];
+        foreach ($firstVerses as $verse) {
+            $groupedVerses[$verse['chapter']][$verse['numv']]=$verse;
+        }
 
         return \View::make('textDisplay.book', [
-            'translation'=>$translation,
-            'reference'=>$translatedRef,
-            'book'=>$book,
-            'firstVerses'=>$firstVerses
+            'translation' => $translation,
+            'reference' => $translatedRef,
+            'book' => $book,
+            'groupedVerses' => $groupedVerses
         ]);
     }
-    
+
 }
