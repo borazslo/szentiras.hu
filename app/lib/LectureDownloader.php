69a7c9ea344b51075ec99a075ffdc8e5a05f0414
Bertalan Fodor <fodber@organum.hu>
2014-04-16 00:07:51 +0200

    Add caching for lecture download.

--- app/lib/LectureDownloader.php
+++ app/lib/LectureDownloader.php
@@ -2,10 +2,12 @@
 
 namespace SzentirasHu\Lib;
 
+use Cache;
 use ErrorException;
 
 class LectureDownloader {
 
+    const LECTURE_CACHE_KEY = 'szentiras.lecture';
     public $date;
 
     public function __construct($date) {
@@ -15,7 +17,14 @@ class LectureDownloader {
     public function getReferenceString() {
         $fn2 = "http://katolikus.hu/igenaptar/{$this->date}.html";
         try {
-            $text = file_get_contents($fn2);
+            $text = Cache::get(self::LECTURE_CACHE_KEY);
+            if (!$text) {
+                $text = file_get_contents($fn2);
+                $tomorrow = \Carbon\Carbon::tomorrow();
+                $tomorrow->setTime(0, 0, 0);
+                Cache::put(self::LECTURE_CACHE_KEY, $text, $tomorrow);
+            }
+
             preg_match('/<!-- helyek:(.*)-->/', $text, $places);
             $referenceString = isset($places[1]) ? trim($places[1]) : '';
             return $referenceString;
