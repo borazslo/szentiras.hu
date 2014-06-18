28b83a5f3ce1043ea47c1c129df7b15f1d603737
Laszlo Elek, SJ <eleklaszlosj@gmail.com>
2014-06-18 22:28:46 +0200

    updateText - rename books

---
+++ app/database/migrations/2014_06_18_201841_rename_books.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameBooks extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::table('books')->where('id', '=', '237')->update(array('abbrev' => 'Tít','link'=>'Tit'));
+        DB::table('books')->where('id', '=', '273')->update(array('abbrev' => 'Júd','link'=>'Jud'));
+        DB::table('books')->where('id', '=', '219')->update(array('abbrev' => '1Tesz','link'=>'1Tesz'));
+        DB::table('books')->where('id', '=', '223')->update(array('abbrev' => '1Tesz','link'=>'1Tesz'));
+        DB::table('books')->where('id', '=', '92')->update(array('abbrev' => 'Ének.Én','link'=>'EnekEn'));
+        DB::table('books')->where('id', '=', '272')->update(array('abbrev' => 'Júd','link'=>'Jud'));
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::table('books')->where('id', '=', '237')->update(array('abbrev' => 'Tit','link'=>'Tit'));
+        DB::table('books')->where('id', '=', '273')->update(array('abbrev' => 'Júdás','link'=>'Judas'));
+        DB::table('books')->where('id', '=', '219')->update(array('abbrev' => '1Tessz','link'=>'1Tessz'));
+        DB::table('books')->where('id', '=', '223')->update(array('abbrev' => '1Tessz','link'=>'1Tessz'));
+        DB::table('books')->where('id', '=', '92')->update(array('abbrev' => 'ÉnekÉn','link'=>'EnekEn'));
+        DB::table('books')->where('id', '=', '272')->update(array('abbrev' => 'Jud','link'=>'Jud'));
+	}
+
+}
