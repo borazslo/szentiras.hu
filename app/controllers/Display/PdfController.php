c33b8cc65b2c8f98a7bcbb11f1b37e4e22fbf597
Bertalan Fodor <fodber@organum.hu>
2014-08-18 11:07:22 +0200

    #64 Generate PDF (beginnings)

---
+++ app/controllers/Display/PdfController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+
+ */
+
+namespace SzentirasHu\Controllers\Display;
+
+
+use Response;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+use \View;
+use SzentirasHu\Lib\Reference\CanonicalReference;
+use SzentirasHu\Lib\Text\TextService;
+use SzentirasHu\Models\Entities\Translation;
+
+class PdfController extends \BaseController {
+
+    /**
+     * @var TextService
+     */
+    private $textService;
+
+    function __construct(TextService $textService)
+    {
+        $this->textService = $textService;
+    }
+
+    public function anyRef($refString)
+    {
+        $ref = CanonicalReference::fromString($refString);
+        $verses = $this->textService->getTranslatedVerses($ref, 3);
+        $content = View::make('textDisplay.pdf.latex')->with('verses', $verses)->render();
+        $workingDir = sys_get_temp_dir();
+        $tmpFileName = tempnam($workingDir, 'szentiras-pdf-');
+        $tmpFile = fopen($tmpFileName, 'w+');
+        fwrite($tmpFile, $content);
+        $builder = new ProcessBuilder(['xelatex', '-interaction=batchmode', '-no-shell-escape', "-output-directory={$workingDir}", $tmpFileName]);
+        $builder->setWorkingDirectory($workingDir);
+        $builder->getProcess()->run(function ($type, $buffer) {
+        });
+        fclose($tmpFile);
+        $response = Response::download(preg_replace('/\.tmp$/', '.pdf', $tmpFileName));
+        $response->headers->set('Content-Type', 'application/pdf');
+        return $response;
+    }
+
+} 
