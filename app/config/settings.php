b2b6d0caa686f7f2291fa6705dd74965526988cb
Bertalan Fodor <fodber@organum.hu>
2014-11-19 15:42:53 +0100

    Return only enabled translations when looking for all.

--- app/config/settings.php
+++ app/config/settings.php
@@ -4,6 +4,7 @@ return [
     'defaultTranslationId' => 3,
     'defaultTranslationAbbrev' => 'KNB',
     'translationAbbrevRegex' => 'KNB|SZIT|UF|KG|BD|knb|szit|uf|kg|bd',
+    'enabledTranslations' => [ 1, 2, 3, 4],
     'audioDirectory' => 'hang',
     'sourceDirectory' => 'tmp',
     'facebookAppId' => '679257202109581',
