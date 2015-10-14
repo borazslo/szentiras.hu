d437eeed5fae66ed33b3a80640dbfb293f508e56
Bertalan Fodor <fodber@organum.hu>
2015-10-14 18:17:56 +0200

    Add more tweaking to sphinx initialization in homestead.

--- after.sh
+++ after.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository -y ppa:builds/sphinxsearch-rel22
 apt-get update
 apt-get install -y sphinxsearch
 apt-get install -y hunspell-hu
 apt-get install -y imagemagick
 mysql -u homestead -psecret < /home/vagrant/szentiras-hu/tmp/database.sql
-export SZENTIRAS_WEBAPP_ENVIRONMENT=local
-cd /home/vagrant/szentiras-hu
-./install.sh
+export APP_HOME=/home/vagrant/szentiras-hu
+
+
+sudo -H -u vagrant bash -c "cd $APP_HOME; export SZENTIRAS_WEBAPP_ENVIRONMENT=local; ./install.sh"
 
 echo '[mysqld]' > /etc/mysql/conf.d/szentiras-hu.cnf
 echo 'query_cache_type=1' >> /etc/mysql/conf.d/szentiras-hu.cnf
 service mysql restart
 
-cp ./deploy/local/sphinx/sphinx.conf /etc/sphinxsearch
-./deploy/local/sphinx/reindex.sh
-
-echo 'START=yes' > /etc/default/sphinxsearch
+service sphinxsearch stop
+cp $APP_HOME/deploy/local/sphinx/sphinx.conf /etc/sphinxsearch
+indexer --all
 service sphinxsearch start
