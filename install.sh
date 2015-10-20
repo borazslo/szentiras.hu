5f5bf85f818cc78433dfb24d7d5c8d90bef11ba2
Bertalan Fodor <fodber@organum.hu>
2015-10-20 16:51:22 +0200

    Travis config: add gulp

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 npm install
-node_modules/.bin/bower --config.interactive=false install
-node_modules/.bin/grunt
 php composer.phar install
 php artisan migrate --env=$SZENTIRAS_WEBAPP_ENVIRONMENT
