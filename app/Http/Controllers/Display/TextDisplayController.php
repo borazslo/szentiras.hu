7cf896aefdb0b9a620ad722c7017bbd3a618681c
Bertalan Fodor <fodber@organum.hu>
2017-03-01 11:36:06 +0100

    Replace spaces instead of urlencode in og:image

--- app/Http/Controllers/Display/TextDisplayController.php
+++ app/Http/Controllers/Display/TextDisplayController.php
@@ -89,7 +89,7 @@ class TextDisplayController extends Controller
             $verseContainers = $this->textService->getTranslatedVerses($canonicalRef, $translation->id);
             $translations = $this->translationRepository->getAllOrderedByDenom();
             return View::make('textDisplay.verses')->with([
-				'canonicalRef' => urlencode($canonicalRef->toString()),
+				'canonicalRef' => str_replace(" ", "%20", $canonicalRef->toString()),
                 'verseContainers' => $verseContainers,
                 'translation' => $translation,
                 'translations' => $translations,
