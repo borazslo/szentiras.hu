a375ea474d2989b875a3818627b526b1a93ec07d
Bertalan Fodor <fodber@organum.hu>
2017-10-13 14:44:50 +0000

    Start adding STL

--- after.sh
+++ after.sh
@@ -9,28 +9,28 @@ else
   echo "Provisioning"
   export DEBIAN_FRONTEND=noninteractive
 
-  add-apt-repository -y ppa:builds/sphinxsearch-rel22
-  apt-get update
-  apt-get install -y sphinxsearch
-  apt-get install -y hunspell
-  apt-get install -y hunspell-hu
-  apt-get install -y graphicsmagick
-  apt-get install -y texlive
-  apt-get install -y texlive-xetex
-  apt-get install -y texlive-latex-extra
-  apt-get install -y texlive-lang-hungarian
-  apt-get install -y fonts-linuxlibertine
-  mysql -u homestead -psecret < /home/vagrant/szentiras-hu/tmp/database.sql
-  echo '[mysqld]' > /etc/mysql/conf.d/szentiras-hu.cnf
-  echo 'query_cache_type=1' >> /etc/mysql/conf.d/szentiras-hu.cnf
-  service mysql restart
+  sudo add-apt-repository -y ppa:builds/sphinxsearch-rel22
+  sudo apt-get update
+  sudo apt-get install -y sphinxsearch
+  sudo apt-get install -y hunspell
+  sudo apt-get install -y hunspell-hu
+  sudo apt-get install -y graphicsmagick
+  sudo apt-get install -y texlive
+  sudo apt-get install -y texlive-xetex
+  sudo apt-get install -y texlive-latex-extra
+  sudo apt-get install -y texlive-lang-hungarian
+  sudo apt-get install -y fonts-linuxlibertine
+  sudo mysql -u homestead -psecret < /home/vagrant/szentiras-hu/tmp/database.sql
+  echo '[mysqld]' | sudo tee /etc/mysql/conf.d/szentiras-hu.cnf
+  echo 'query_cache_type=1' | sudo tee --append /etc/mysql/conf.d/szentiras-hu.cnf
+  sudo service mysql restart
   export APP_HOME=/home/vagrant/szentiras-hu
   sudo -H -u vagrant bash -c "cd $APP_HOME; export SZENTIRAS_WEBAPP_ENVIRONMENT=local; ./install.sh"
 
-  service sphinxsearch stop
-  cp $APP_HOME/deploy/local/sphinx/sphinx.conf /etc/sphinxsearch
-  indexer --all
-  service sphinxsearch start
+  sudo service sphinxsearch stop
+  sudo cp $APP_HOME/deploy/local/sphinx/sphinx.conf /etc/sphinxsearch
+  sudo indexer --all
+  sudo service sphinxsearch start
   touch $PROVISIONED;
 fi
-
+ 
--- config/translations.php
+++ config/translations.php
@@ -54,5 +54,12 @@ return [
             'xref' => [2021]
         ],
         'textSource' => env('TEXT_SOURCE_RUF')
+    ],
+    'STL' => [
+        'verseTypes' =>
+        [
+            'text' => [901],
+            'footnote' => [2001]
+        ]
     ]
 ];
